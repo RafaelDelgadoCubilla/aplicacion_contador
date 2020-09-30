92cf73dee29baa59fd809b7dd4efd2916c49fd14
Rafael Delgado <rafael.delgado.cubilla@iesjandula.es>
2020-09-30 11:56:31 +0200

    Antes de convertir en StateFullWidget

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
 
-  final miestilo = TextStyle(fontSize: 25);
+  final _miestilo = TextStyle(fontSize: 25);
+  final _contador=0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +17,15 @@ class HomePage extends StatelessWidget{
         child:Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Número de veces que has pulsado el botón', style: miestilo),
-            Text('0', style: miestilo),
+            Text('Número de veces que has pulsado el botón', style: _miestilo),
+            Text('$_contador', style: _miestilo),
           ],
         )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print ('Hola Mundo');
+          //_contador++;
         },
         child: Icon(Icons.add),
       ),
